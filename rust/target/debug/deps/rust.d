ecf4949de91af86f0e614acbd80e016fb224a292
Alejandro Rodríguez González <arodriguez.gonzalez@knowmadmood.com>
2025-04-29 17:43:48 +0200

    Añadiendo configuraciones

---
+++ rust/target/debug/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\deps\rust.exe: src/main.rs
+
+C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\deps\rust.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\rust.exe: C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\src\main.rs
