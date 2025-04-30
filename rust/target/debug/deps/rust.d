9a352ff34a3fc8a35bc05ae1230290aee855b9a0
Alejandro Rodríguez González <arodriguez.gonzalez@knowmadmood.com>
2025-04-30 12:08:42 +0200

    Eliminado directorio target (Se genera automaticamnete al compilar)

--- rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\deps\rust.exe: src/main.rs
-
-C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\deps\rust.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\target\debug\rust.exe: C:\Users\arodriguez.gonzalez\Downloads\Eliminar\rust\rust\src\main.rs
